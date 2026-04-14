7f2db2730be5f1cb4b91a3f25281c009b066489c
Tony Coppola <tony@oliver.com>
2026-04-14 13:06:52 +0100

    fix: wire Header into placeholder pages

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,3 +1,10 @@
+import Header from '../components/Header'
+
 export default function AdminPage() {
-  return <div className="p-8 font-sans">Admin</div>
+  return (
+    <div className="min-h-screen bg-gray-50 font-sans">
+      <Header />
+      <div className="p-8">Admin</div>
+    </div>
+  )
 }
--- src/pages/SurveyPage.tsx
+++ src/pages/SurveyPage.tsx
@@ -1,3 +1,10 @@
+import Header from '../components/Header'
+
 export default function SurveyPage() {
-  return <div className="p-8 font-sans">Survey</div>
+  return (
+    <div className="min-h-screen bg-gray-50 font-sans">
+      <Header />
+      <div className="p-8">Survey</div>
+    </div>
+  )
 }
